cbf0203b9f847d4074ac9e1f5b45f769733a9223
abimbola7 <abimbolababajide6@gmail.com>
2025-03-23 07:46:58 -0500

    feat: enhance Section9 layout with new icon and Umplex SVG, update GSAP animations, and improve styling in globals.css

--- src/app/(components)/section9.jsx
+++ src/app/(components)/section9.jsx
@@ -6,6 +6,8 @@ import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import ScrollTrigger from 'gsap/ScrollTrigger'
 import Staylowkey from '@/assets/svgs/staylowkey'
+import IconSvg from '@/assets/svgs/iconsvg'
+import Umplex from '@/assets/svgs/umplex'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -15,8 +17,6 @@ const Section9 = () => {
   const { contextSafe } = useGSAP(()=>{
     const section9 = document.querySelector("#section9");
     const motto = document.querySelector(".motto1");
-    const slogan  = document.querySelectorAll(".slogan1");
-    const miniSlogan  = document.querySelector(".minislogan1");
     const mainSection  = document.querySelector(".mainsection1");
     timeline.current = gsap.timeline({
       scrollTrigger : {
@@ -31,29 +31,29 @@ const Section9 = () => {
       duration : 1.2,
       ease : "power4.out",
     },0)
-    .from(slogan, {
-      x: -250,
-      opacity : 0,
-      duration : 1.2,
-      ease : "power4.out",
-    },0)
+    // .from(slogan, {
+    //   x: -250,
+    //   opacity : 0,
+    //   duration : 1.2,
+    //   ease : "power4.out",
+    // },0)
 
-    gsap.from(miniSlogan, {
-      x : -200,
-      opacity : 0,
-      delay : 0,
-      ease: "bounce.out",
-      duration : .5,
-      scrollTrigger : {
-        trigger : mainSection,
-        toggleActions : "play none none reset",
-      }
-    })
+    // gsap.from(miniSlogan, {
+    //   x : -200,
+    //   opacity : 0,
+    //   delay : 0,
+    //   ease: "bounce.out",
+    //   duration : .5,
+    //   scrollTrigger : {
+    //     trigger : mainSection,
+    //     toggleActions : "play none none reset",
+    //   }
+    // })
   }, {
     // scope : section9Ref
   })
   return (
-    <div className="relative flex items-center justify-center w-full h-[120vh] overflow-hidden" id="section9">
+    <div className="relative flex items-center justify-center w-full h-[120vh] overflow-hidden px-3 flex-col" id="section9">
       <div className='absolute top-0 -left-10 motto1'>
         <Staylowkey
         width={"900"}
@@ -61,18 +61,32 @@ const Section9 = () => {
         fill={"#0D0D0D"}
         />
       </div>
-      <div className='relative flex flex-col justify-start w-full max-w-6xl px-20 text-white gap-y-16 mainsection1'>
-        <div className='absolute top-0 left-0 uppercase text-[#ffffff80] text-[.7rem] transition-transform -rotate-90 duration-500 translate-y-8 font-extrabold tracking-wider -translate-x-7 minislogan1'>
-          our slogan
-        </div>
-        <div className='flex flex-col gap-y-6 slogan1'>
-          <h1 className='font-semibold tracking-wider text-7xl'>STAY LOW KEY</h1>
-          <p className='text-3xl font-[500]'>&apos;Stay focused on the end goal until you succeed&apos;</p>
-        </div>
+      <div className="w-full max-w-6xl relative z-[10] mx-auto ">
+        <div className='flex items-center justify-between py-12 border-b-[.1px] border-b-[#262626]'>
+          <div className=''>
+            <img src="/icon.png" alt="" className="w-[95px] h-[95px]" />
+          </div>
 
-        <div className='space-y-6 slogan1'>
-          <h2 className='text-2xl'>Live with your own values, not others.</h2>
-          <p className='text-[#ffffff80] text-sm leading-loose font-medium'>STAY LOW KEY means「 understand 」and 「 live by believing in one&apos;s values without matching the values of others.  Everybody has a belief or story, and it is worth giving back to society only by that person. ALL-IN is formed by each person who lives with his own values</p>
+          <div className='flex items-center space-x-12 text-xs font-bold tracking-wide uppercase text-white/50'>
+            {
+              [
+                ["ABOUT"],
+                ["SERVICE"],
+                ["WORKS"],
+                ["COMPANY"],
+                ["CAREER"],
+                ["NEWS"],
+                ["CONTACT"]
+              ].map(([item]) => (
+                <p key={item} className='transition-colors duration-200 cursor-pointer hover:text-white'>{item}</p>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+      <div className='flex flex-col items-end justify-center relative z-[10] border w-full'>
+        <div className='flex flex-row items-center space-x-3 text-white'>
+          <Umplex />
         </div>
       </div>
     </div>
