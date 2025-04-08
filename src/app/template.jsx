064e6ddc66907eeff5f20cb9b84194e1728ba647
abimbola7 <abimbolababajide6@gmail.com>
2025-04-07 20:40:40 -0500

    refactor: streamline animation logic in Template component and enhance structure for improved readability

--- src/app/template.jsx
+++ src/app/template.jsx
@@ -5,50 +5,48 @@ import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 import { useDispatch } from 'react-redux'
 import { showHeader } from '@/store/uislice'
+import { animatePageIn } from './utils/animate'
 
 
 const Template = ({ children }) => {
-  const openingRef = React.useRef(null);
   const dispatch = useDispatch()
-  const timeline = React.useRef()
-  useGSAP(()=>{
-    timeline.current = gsap.timeline({
-      defaults : {
-        // immediateRender : false,
-        duration : .6,
-        delay : 1,
-        opacity : 1,
-        onComplete : () => {
-          dispatch(showHeader())
-        }
-      }
-    })
-    // .set(".red", {
-    //   xPercent : -101, 
-    //   autoAlpha : 1
-    // }, 0)
-    .from(openingRef.current, {
-      xPercent : -100, scaleX : 1
-    }, 0)
-    .to(".iconn", {
-      x : 100,
-      ease : "power2.out",
-      opacity : 0,
-      duration : .5
-    }, 0)
-    // .to(".red", {
-    //   autoAlpha : 1,
-    //   xPercent : 200,
-    //   ease : "power4.out",
-    // }, 0)
-    .to(openingRef.current, {
-      scaleX : 0, transformOrigin : "right"
-    }, 0.2)
-    .to(".overlay", {
-      zIndex : -100
-    }, .7)
-  })
+  // useGSAP(()=>{
+  //   const openingRef = document.querySelector(".opening");
+  //   const iconn = document.querySelector(".iconn");
+  //   const overlay = document.querySelector(".overlay");
+  //   gsap.timeline({
+  //     defaults : {
+  //       // immediateRender : false,
+  //       duration : .6,
+  //       delay : 1,
+  //       opacity : 1,
+  //       onComplete : () => {
+  //         dispatch(showHeader())
+  //       }
+  //     }
+  //   })
+  //   .from(openingRef, {
+  //     xPercent : -100, scaleX : 1
+  //   }, 0)
+  //   .to(iconn, {
+  //     x : 100,
+  //     ease : "power2.out",
+  //     opacity : 0,
+  //     duration : .5
+  //   }, 0)
+  //   .to(openingRef, {
+  //     scaleX : 0, transformOrigin : "right"
+  //   }, 0.2)
+  //   .to(overlay, {
+  //     zIndex : -100
+  //   }, .7)
+  // })
 
+  useGSAP(() => {
+    animatePageIn(dispatch);
+  }, {
+  dependencies : [dispatch]
+  });
  
   return (
     <>
@@ -56,10 +54,8 @@ const Template = ({ children }) => {
         <div className='flex items-center justify-center w-full h-full iconn'>
           <img src='/icon.png' alt='img' className='object-cover w-20 h-20 tab:w-fit tab:h-fit'/>
         </div>
-        {/* <div className='absolute top-0 left-0 w-3/4 h-full bg-[#B30606] opacity-0 red'></div> */}
         <div 
-        ref={openingRef}
-        className='absolute top-0 left-0 w-full !p-0 !m-0 h-full bg-[#B30606] opacity-0'></div>
+        className='absolute top-0 left-0 w-full !p-0 !m-0 h-full bg-[#B30606] opening opacity-0'></div>
       </div>
       { children }
     </>
---
+++ src/app/utils/animate.js
@@ -0,0 +1,39 @@
+import gsap from "gsap";
+import { showHeader } from "@/store/uislice";
+
+
+export const animatePageIn = (dispatch) => {
+  const openingRef = document.querySelector(".opening");
+  const iconn = document.querySelector(".iconn");
+  const overlay = document.querySelector(".overlay");
+  const tl = gsap.timeline({
+    defaults : {
+      // immediateRender : false,
+      duration : .6,
+      delay : 1,
+      opacity : 1,
+      onComplete : () => {
+        dispatch(showHeader())
+      }
+    }
+  }).from(openingRef, {
+      xPercent : -100, scaleX : 1
+    }, 0)
+    .to(iconn, {
+      x : 100,
+      ease : "power2.out",
+      opacity : 0,
+      duration : .5
+    }, 0)
+    .to(openingRef, {
+      scaleX : 0, transformOrigin : "right"
+    }, 0.2)
+    .set(overlay, {
+      zIndex : -100
+    }, .7)
+}
+
+
+export const animatePageOut = () => {
+
+}
