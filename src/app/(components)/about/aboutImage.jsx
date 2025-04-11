2f70ffe76c9e64018c485d2166af3f00899c6d2a
abimbola7 <abimbolababajide6@gmail.com>
2025-04-11 04:49:45 -0500

    commit

--- src/app/(components)/about/aboutImage.jsx
+++ src/app/(components)/about/aboutImage.jsx
@@ -1,13 +1,14 @@
+import Motto from '@/assets/svgs/motto'
 import Staylowkey from '@/assets/svgs/staylowkey'
 import React from 'react'
 
 const AboutImage = () => {
   return (
-    <div className="bg-[#181818] relative flex w-full h-[120vh] py-20 tab:py-0 tab:h-[50rem] overflow-hidden" id="section4">
-      <div className='relative flex flex-col justify-start w-full max-w-3xl text-white tab:px-0 tab:max-w-[1440px] gap-y-5 tab:gap-y-16 mx-auto minisection2'>
+    <div className="bg-[#181818] relative flex w-full h-[95rem] py-20 tab:py-0 tab:h-[70rem] overflow-hidden" id="section4">
+      <div className='relative flex items-end w-full max-w-3xl text-white tab:px-0 tab:max-w-[1440px] gap-y-5 tab:gap-y-16 mx-auto minisection2'>
         <div className='absolute bottom-0 -left-[13%] tab:top-0 tab:bottom-auto motto'>
           <Staylowkey
-          className="w-[283x] h-[296px] tab:w-[900px] tab:h-[850px]"
+          className="w-[283x] h-[296px] tab:w-[900px] tab:h-[850px] z-[10]"
           width={"auto"}
           height={"auto"}
           fill={"#1D1D1D"}
@@ -16,29 +17,6 @@ const AboutImage = () => {
 
         {/* image section */}      
         <div className="absolute w-full h-fit max-w-full tab:max-w-[52%] top-0 left-0 tab:right-0 tab:left-auto">
-          {/* <div className='border h-fit w-full max-w-4xl mx-auto tab:max-w-full relative aspect-[3/2]'>
-            <img 
-            src="https://allhero.co.jp/wp-content/themes/allhero.co.jp/assets/images/about/slogan02.jpg" 
-            alt="img1"  
-            className='absolute right-0 top-0 h-auto w-[56%] z-10'/>
-            <img 
-            src="https://allhero.co.jp/wp-content/themes/allhero.co.jp/assets/images/about/slogan01.jpg" 
-            alt="img2"  
-            className='absolute left-5 top-36 h-auto w-[48%] z-[9]'/>
-            <img 
-            src="https://allhero.co.jp/wp-content/themes/allhero.co.jp/assets/images/about/slogan05.jpg" 
-            alt="img2"  
-            className='absolute left-28 mt-[46%] h-auto w-[35%] z-[15]'/>
-            <img 
-            src="https://allhero.co.jp/wp-content/themes/allhero.co.jp/assets/images/about/slogan03.jpg" 
-            alt="img2"  
-            className='absolute right-0 mt-[50%] h-auto w-[36%] z-[11]'/>
-            <img 
-            src="https://allhero.co.jp/wp-content/themes/allhero.co.jp/assets/images/about/slogan04.jpg" 
-            alt="img2"  
-            className='absolute left-[38%] mt-[60%] h-auto w-[35%] z-[12]'/>
-          </div> */}
-
           <div className="relative w-full max-w-[1000px] aspect-[3/2] mx-auto">
             <img 
               src="https://allhero.co.jp/wp-content/themes/allhero.co.jp/assets/images/about/slogan02.jpg" 
@@ -68,8 +46,22 @@ const AboutImage = () => {
           </div>
         </div>
 
-        <div className=''>
+        <div className='max-w-4xl w-full relative z-[11] mx-auto flex flex-col gap-20 pb-20'>
+          <Motto
+          height={"auto"}
+          width={"auto"}
+          className={"w-[200px] h-[250px] tab:w-[340px] tab:h-[380px]"}
+          />
 
+          <div className='flex flex-col gap-4'>
+            <h2 className="text-2xl font-medium tracking-wider">
+              Conservatively and noble
+            </h2>
+
+            <p className="text-[15px] text-[hsla(0,0%,100%,.5)] tracking-wider">
+            STAY LOW KEY has two meanings. First, it means &apos;being discreet&apos; and the other means &apos;believing in the values of others, believing in your values and living.&apos; These two are not dorsy. I can be discreet because I am aware that I am only a part of the world, and at the same time it is a proof that I am proud to be one that does not look like anyone else. ALL-IN provides society with the value that can be reduced because it is one that everyone has.
+            </p>
+          </div>
         </div>
 
       </div>
